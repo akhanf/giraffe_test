e69824c67f164e40434942edbd4383808b1fff1f
Ali Khan <akhanf@users.noreply.github.com>
2019-11-22 10:01:40 -0500

    initial workflow

---
+++ GIRAFFE/code/workflow.py
@@ -0,0 +1,29 @@
+#This is a Nipype generator. Warning, here be dragons.
+#!/usr/bin/env python
+
+import sys
+import nipype
+import nipype.pipeline as pe
+
+import nipype.interfaces.io as io
+import nipype.interfaces.fsl as fsl
+
+#Flexibly collect data from disk to feed into workflows.
+io_select_files = pe.Node(io.SelectFiles(templates={}), name='io_select_files')
+
+#Wraps the executable command ``bet``.
+fsl_bet = pe.Node(interface = fsl.BET(), name='fsl_bet')
+
+#Generic datasink module to store structured outputs
+io_data_sink = pe.Node(interface = io.DataSink(), name='io_data_sink')
+
+#Create a workflow to connect all those nodes
+analysisflow = nipype.Workflow('MyWorkflow')
+analysisflow.connect(io_select_files, "anat", fsl_bet, "in_file")
+analysisflow.connect(fsl_bet, "out_file", io_data_sink, "BET_results")
+
+#Run the workflow
+plugin = 'MultiProc' #adjust your desired plugin here
+plugin_args = {'n_procs': 1} #adjust to your number of cores
+analysisflow.write_graph(graph2use='flat', format='png', simple_form=False)
+analysisflow.run(plugin=plugin, plugin_args=plugin_args)
